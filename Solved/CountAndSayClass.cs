817ee78a6d55e9ee8fc5cf6a2d25d4ed168c8ec5
maxim.petrochenko <maxim.petrochenko@effective-soft.com>
2024-05-08 06:23:28 +0300

    CountAndSay https://leetcode.com/problems/count-and-say/description/

---
+++ Solved/CountAndSayClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solved
+{
+    internal class CountAndSayClass
+    {
+        public string CountAndSay(int n)
+        {
+            if(n == 1) return "1";
+            return rec(n - 1);
+        }
+
+        public string rec(int n)
+        {
+            if(n == 1) return "11";
+            var s = rec(n - 1);
+            int i = 0;
+            var sb = new StringBuilder();
+            while(i < s.Length)
+            {
+                int cnt = 1;
+                int j = i + 1;
+                while(j < s.Length && s[i] == s[j]) { j++; cnt++; }
+                sb.Append(cnt)
+                  .Append(s[i]);
+                i = j;
+            }
+            return sb.ToString();
+        }
+    }
+}
